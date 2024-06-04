a4c85cde96532d2a1656bb85930e9c73fbcf0603
Sakar Aryal <techiesakar@gmail.com>
2024-06-04 20:18:28 +0545

    day 25 completed

--- day25/main.js
+++ day25/main.js
@@ -1,26 +1,66 @@
+const tableBody = document.querySelector('tbody')
+const tdClass = "border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400"
+
 const payload = {
     currency: "USD",
     sort: "rank",
     order: "ascending",
     offset: 0,
     limit: 5,
-    meta: false
+    meta: true
 }
 
 const fetchData = async () => {
-    const res = await fetch(new Request('https://api.livecoinwatch.com/coins/list'), {
+    tableBody.innerHTML = ""
+    const res = await fetch("https://api.livecoinwatch.com/coins/list", {
         method: 'POST',
         body: JSON.stringify(payload),
 
         headers: new Headers({
             'content-type': 'application/json',
-            'x-api-key': 'put your api'
+            'x-api-key': '5acdda65-ebe6-430c-9858-ad4552cba141'
         })
     })
 
     const data = await res.json()
-    console.log(data)
+
+    data.map((item, i) => {
+        // for sn
+        let tr = document.createElement('tr')
+        let tdSN = document.createElement('td')
+        tdSN.className = tdClass
+        tdSN.textContent = i + 1
+
+        // for image
+        let tdImage = document.createElement('td')
+        tdImage.className = tdClass
+        let imgEle = document.createElement('img')
+        imgEle.src = item.png64
+        imgEle.classList.add("size-6")
+        tdImage.append(imgEle)
+
+        // for coin name
+        let tdName = document.createElement("td")
+        tdName.className = tdClass
+        tdName.textContent = item.name
+
+        // for rate
+        let tdRate = document.createElement("td")
+        tdRate.className = tdClass
+        tdRate.textContent = `$ ${parseFloat(item.rate).toFixed(2)}`
+
+
+        // const tdIcon = document.createElement('td')
+        // const tdCoin = document.createElement('td')
+        // const tdPrice = document.createElement('td')
+
+        tr.append(tdSN, tdImage, tdName, tdRate)
+        tableBody.append(tr)
+    })
+
 }
 
 fetchData()
+
+setInterval(fetchData, 10 * 1000)
 
