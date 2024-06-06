44d4e787a06031ba87124ddc86e5e2fcab16140e
Sakar Aryal <techiesakar@gmail.com>
2024-06-06 09:57:11 +0545

    show hide password

--- day27/main.js
+++ day27/main.js
@@ -1,3 +1,36 @@
+const showHideBtns = document.querySelectorAll(".showHide")
+
+
 function validate() {
     alert("validating")
+}
+
+
+for (i = 0; i < showHideBtns.length; i++) {
+    // showHideBtns[i].addEventListener("click", function () {
+    //     console.log(this.name, "hey this")
+    // })
+
+    showHideBtns[i].addEventListener("click", function () {
+        // console.log(this.name)
+        showHidePassword(this.name)
+    })
+
+
+
+
+}
+
+
+function showHidePassword(buttonName) {
+    let inputField
+    if (buttonName === "confirmPassButton") {
+        inputField = "confirmPassword"
+    }
+
+    else if (buttonName === "showPassBtn") {
+        inputField = "password"
+    }
+    document.forms["signUpForm"][inputField].type = document.forms["signUpForm"][inputField].type === "password" ? "text" : "password"
+
 }
